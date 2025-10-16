8e4d2ddc25b95aeb2217d34c377fd28e49545c69
Mojib Rsm <mojibrsm@gmail.com>
2025-10-16 17:24:26 +0000

    ha

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -241,6 +241,8 @@ export async function uploadImage(formData: FormData): Promise<{ url?: string; e
             headers: {
                 "Accept": "application/json",
                 "X-Requested-With": "XMLHttpRequest",
+                "Origin": "http://img.bdedu.me",
+                "Referer": "http://img.bdedu.me/en/user/gallery"
             }
         });
 
